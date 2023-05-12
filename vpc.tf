8e0b1b242b43b4f6368c608caa2c4883b577f485
A-asine <asine.archanasaini@gmail.com>
2023-05-12 14:00:45 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,18 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google"
-    version = "~> 7.0"
+    version = "~> 3.4.0"
 
     project_id   = "var.project"
     network_name = "gaurav-network"
     routing_mode = "GLOBAL"
 
     subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+    {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+    }
+    ]
 
     secondary_ranges = {
         subnet-01 = [
